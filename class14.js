f4d17b5b408f9a98d2f08ff6065af8d9b5d7dde4
vignesh57001 <109303622+vignesh57001@users.noreply.github.com>
2022-09-01 02:15:15 -0700

    Add files via upload

---
+++ class14.js
@@ -0,0 +1,8 @@
+function foo() {
+    var res=  document.getElementById("fname").value;
+    var res1= document.getElementById("mname").value;
+    var res2=  document.getElementById("lname").value;
+    var res3= document.getElementById("email").value;
+
+      console.log(res,res1,res2,res3);
+}
